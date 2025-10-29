2e7999b97a59273c972485bd47944b593665c8d4
LeeEunBi <captain.dev.b@gmail.com>
2025-10-29 12:25:07 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { appLogin, setClipboardText } from "@apps-in-toss/web-framework";
+// import { Button } from "@toss/tds-mobile";
 
 function App() {
+  const [state, setState] = useState({
+    code: "",
+    referrer: "",
+  });
+  const [locationInfo, setLocationInfo] = useState(null);
+  const login = async () => {
+    console.log("로그인 시작");
+    try {
+      const res = await appLogin();
+
+      console.log("res", res);
+
+      setState({
+        code: res.authorizationCode,
+        referrer: res.referrer,
+      });
+    } catch (e) {
+      console.error(e);
+      console.log("AccessToken을 가져오는 중 문제가 발생했어요.");
+    } finally {
+      console.log("완료");
+    }
+  };
+
+  const clip = async () => {
+    try {
+      await setClipboardText(state.code);
+      alert("텍스트가 복사됐어요!");
+    } catch (error) {
+      alert("권한거부");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      window.location 값 <br />
+      <pre>
+        {locationInfo ? JSON.stringify(locationInfo, null, 2) : "Loading..."}
+      </pre>
+      <br />
+      <hr />
+      <br />
+      토스 로그인 값 확인
+      <br />
+      <br />
+      <button type="button" display="block" onClick={login}>
+        토스 로그인 하기
+      </button>
+      <br />
+      <br />
+      <br />
+      authorizationCode: {state.code}
+      <br />
+      <br />
+      referrer: {state.referrer}
+      <br />
+      <br />
+      {state.code ? (
+        <button type="button" display="block" onClick={clip}>
+          authorizationCode 복사하기
+        </button>
+      ) : (
+        ""
+      )}
+      <br />
+      <hr />
+      <br />
+      <br />
+      <br /> <br /> <br /> <br /> <br /> <br /> <br />
+    </>
   );
 }
 
